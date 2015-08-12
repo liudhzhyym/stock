90a822b666fd75e80ef36d66d0a2e4b6775b9d0f
liudhzhyym <mengwei97@whut.edu.cn>
2015-08-12 13:50:18 +0800

    update

---
+++ CodeIgniter_2.2.0/application/scripts/query.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function random()
+{
+    min=$1
+    max=$2-$1
+    num=$RANDOM
+    ((retnum=num%max+min))
+    #进行求余数运算即可
+    echo $retnum
+    #这里通过echo 打印出来值，然后获得函数的，stdout就可以获得值
+}
+
+cd ../../
+if [ ! -f index.php ] ; then
+	echo "index.php is not exist!"
+fi
+
+index=0
+for strategy in `cat application/data/strategy2load.conf`; do
+	for day in `cat application/data/timeList.conf` ; do
+		/home/work/osp/php/bin/php index.php stock queryDataByIndexAndDay $index $day
+		rand=$(random 2 5)
+		echo "sleep [$rand]s"
+		sleep $rand
+	done
+	((index++))
+done
+
+
+
